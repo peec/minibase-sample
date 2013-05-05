5fc9c0c8735d1969c2d3bc2897ce7a1367cc779f
peec <kjelkenes@gmail.com>
2013-05-05 04:10:27 +0200

    Add Assetic plugin to default. Also update template.html to use assetic. Use mb:assetic:dump to dump files.

--- www/index.php
+++ www/index.php
@@ -1,7 +1,8 @@
 <?php
 require __DIR__ .'/../vendor/autoload.php';
 
-$mb = Minibase\MB::create()->loadConfigFile(__DIR__ . '/../app/app.json', __DIR__ . '/../app');
+$mb = Minibase\MB::create(__DIR__)
+	->loadConfigFile(__DIR__ . '/../app/app.json', __DIR__ . '/../app');
 
 session_start();
 $mb->start();
