319f96649384035373e8952ea5bb0c7ccb08ccff
peec <kjelkenes@gmail.com>
2013-05-11 22:13:18 +0200

    Update to latest version

--- www/index.php
+++ www/index.php
@@ -4,5 +4,4 @@ require __DIR__ .'/../vendor/autoload.php';
 $mb = Minibase\MB::create(__DIR__)
 	->loadConfigFile(__DIR__ . '/../app/app.json', __DIR__ . '/../app');
 
-session_start();
 $mb->start();
